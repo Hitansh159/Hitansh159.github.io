2ca29861513915816629e6940fb2be987c64baa7
Hitansh Doshi <hkd159@gmail.com>
2024-09-01 17:17:39 +0530

    fix animation behaviour

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -20,9 +20,9 @@ export default function Experience() {
       >
         {experienceData.jobs.map((job, index) => (
           <Grid xs={1} sm={4} md={5} lg={4} key={index}>
-            <motion.div 
-              whileInView={{x:["-50vh", "0px"], opacity:[0,1]}}
-              transition={{duration: index*0.2}}
+            <motion.div
+              whileInView={{ x: ["50vh", "0px"], opacity: [0, 1] }}
+              transition={{ duration: 0.2 }}
             >
               <GradientCard imgSrc={job.imgSrc} cardTitle={job.title} cardSubTitle={job.role} type="job" modalData = {job}/>
             </motion.div>
